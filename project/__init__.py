c7057924a9c97ffd145e66a8de63ebcdacfd67e3
Drew J <35353143+CatsAreEvil@users.noreply.github.com>
2019-08-21 19:21:44 -0400

    Setup project

---
+++ project/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_sqlalchemy  import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import os
+import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+login_manager = LoginManager()
+login_manager.init_app(app)
+app.config.from_object(os.environ['APP_SETTINGS'])
+bcryptObj = Bcrypt(app)
+localSystem = None
+
+db = SQLAlchemy(app)
+
+from project.home.views import home_blueprint
+
+app.register_blueprint(home_blueprint)
+
+# from project.models import User
+
+# login_manager.login_view = "users.login"
+
+# @login_manager.user_loader
+# def load_user(user_id):
+#     try:
+#         user = User.query.filter(User.id == int(user_id)).first()
+#     except:
+#         user = None
+#     return user
